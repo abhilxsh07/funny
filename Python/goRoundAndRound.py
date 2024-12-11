cc309d60ae9f600b499d14d6470b73e655965d61
Abhilash Kar <100826642+abhilxsh07@users.noreply.github.com>
2024-12-11 20:24:46 +0530

    Added context

--- Python/goRoundAndRound.py
+++ Python/goRoundAndRound.py
@@ -1,3 +1,5 @@
+#this code's only goal is to recursively call the same function over and over again. a bit memory consuming but the larger goal is to understand how easy it is in large scale applications to make this blunder.
+
 def f():
     global c
     print("Hello World!", c)
